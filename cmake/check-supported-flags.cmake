0faa1ce7d2d5ea0ba54dd00b7272a273c24b90b5
Kaido Kert <kaidokert@gmail.com>
2016-01-09 19:25:04 -0800

    Clean make up for correct variable names

--- cmake/check-supported-flags.cmake
+++ cmake/check-supported-flags.cmake
@@ -1,15 +1,16 @@
 include(CheckCXXSourceCompiles)
 include(CMakeCheckCompilerFlagCommonPatterns)
 
-include(compiler-flags.cmake)
+include(cmake/compiler-flags.cmake)
 
 macro(check_supported_flags OUTPUT_VAR)
   CHECK_COMPILER_FLAG_COMMON_PATTERNS(_CheckCXXCompilerFlag_COMMON_PATTERNS)
   foreach(flag ${GCC_EXTRA_WARNINGS} ${CLANG_EXTRA_WARNINGS})
     set(CMAKE_REQUIRED_DEFINITIONS "-W${flag}")
-    CHECK_CXX_SOURCE_COMPILES("int main() { return 0; }" flag-${flag}-result
+    string(REPLACE "=" "-" result_var "flag-${flag}-result" )
+    CHECK_CXX_SOURCE_COMPILES("int main() { return 0; }" ${result_var}
       FAIL_REGEX ${_CheckCXXCompilerFlag_COMMON_PATTERNS})
-    if(${flag-${flag}-result})
+    if(${result_var})
       list(APPEND CXX_ALL_WARNINGS "${flag}")
     endif()
   endforeach()
