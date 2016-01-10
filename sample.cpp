f5aa7aab8d3a11eabad971a50f737b2d53cae8c0
kaido <kaidokert@gmail.com>
2016-01-10 11:04:18 -0800

    Move MSVC disabled warning flags out to project files

--- sample.cpp
+++ sample.cpp
@@ -1,11 +1,6 @@
-#pragma warning( disable : 4350 4710 )
-#pragma warning(push, 3)
 #include <iostream>
-#pragma warning(pop)
 
 int main(int , char **) {
     std::cout << "Hello CI!" << std::endl;
     return 0;
 }
-
-#pragma warning( disable : 4514)
