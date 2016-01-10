a94522c8cf0722ff11af4be71747fbc28ec90d62
kaido <kaidokert@gmail.com>
2016-01-10 10:19:18 -0800

    Push Msvc

--- sample.cpp
+++ sample.cpp
@@ -1,6 +1,11 @@
+#pragma warning( disable : 4350 )
+#pragma warning(push, 3)
 #include <iostream>
+#pragma warning(pop)
 
 int main(int , char **) {
-    std::cout << "Hello CI!";
+    std::cout << "Hello CI!" << std::endl;
     return 0;
 }
+
+#pragma warning( disable : 4710 4514)
